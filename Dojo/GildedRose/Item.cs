4cda3c6915460a641b426559d23ca2a18ad6f9ad
Pandit Adhilaga-Dres <pandit.dres@gmail.com>
2022-03-15 14:43:01 +0000

    init

---
+++ Dojo/GildedRose/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace Dojo.GildedRose
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+    }
+}
