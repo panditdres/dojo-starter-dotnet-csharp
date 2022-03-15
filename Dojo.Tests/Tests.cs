63c1873ea31579cd4a05260b576d5f2a213d8e5c
Pandit Adhilaga-Dres <pandit.dres@gmail.com>
2022-03-15 15:26:53 +0000

    legendary item test

--- Dojo.Tests/Tests.cs
+++ Dojo.Tests/Tests.cs
@@ -28,6 +28,34 @@ namespace Dojo.Tests
         [Fact]
         public void GivenSellInDatePassed_QualityDegradesTwice()
         {
+            var (sut, items) = Arrange(Item("Orange", 10, -1));
+
+            sut.UpdateQuality();
+
+            Assert.Equal(8, items[0].Quality);
+            Assert.Equal(-2, items[0].SellIn);
+        }
+
+        [Fact]
+        public void UpdateQuality_AgedBrieIfQualityLessThan50ThenIncrementsBy1()
+        {
+            // Arrange
+            var (sut, items) = Arrange(Item("Aged Brie", 49, 10));
+
+            sut.UpdateQuality();
+            
+            Assert.Equal(50, items[0].Quality);
+        }
+
+        [Fact]
+        public void GivenLegendaryItem_ThenQualityAndSellInDontChange()
+        {
+            var (sut, items) = Arrange(Item("Sulfuras, Hand of Ragnaros", 40, 0));
+
+            sut.UpdateQuality();
+
+            Assert.Equal(40, items[0].Quality);
+            Assert.Equal(0, items[0].SellIn);
         }
 
         Item Item(string name, int quality, int sellIn) => new Item
@@ -41,34 +69,6 @@ namespace Dojo.Tests
         {
             var itemList = items.ToList();
             return (new GildedRose.GildedRose(itemList), itemList);
-        }
-
-        public void UpdateQuality_AgedBrieIfQualityLessThan50ThenIncrementsBy1()
-        {
-            // Arrange
-            var itemList = new List<Item>();
-
-            var item = new Item()
-            {
-                Name = "Aged Brie",
-                Quality = 49,
-                SellIn = 10
-            };
-            
-            itemList.Add(item);
-
-            // Act
-            var guildedRose = new GildedRose.GildedRose(itemList);
-            guildedRose.UpdateQuality();
-
-            // Assert
-            Assert.Equal(50, itemList[0].Quality);
-        }
-
-        [Fact]
-        public void GivenSulfuras_ThenQualityDoesntChange()
-        {
-
         }
     }
 }
