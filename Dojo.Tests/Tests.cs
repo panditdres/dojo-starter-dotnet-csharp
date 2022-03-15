8de1ee45755823896d6cd5898501ef338bbbdf75
Pandit Adhilaga-Dres <pandit.dres@gmail.com>
2022-03-15 14:56:42 +0000

    First test

--- Dojo.Tests/Tests.cs
+++ Dojo.Tests/Tests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Dojo.GildedRose;
 using Xunit;
 
 namespace Dojo.Tests
@@ -9,6 +11,37 @@ namespace Dojo.Tests
         public void StarterTest()
         {
             Assert.True(Solution.IsSetup);
+        }
+
+        [Fact]
+        public void GivenEndOfDay_ReduceQualityAndSellIn()
+        {
+            // Arrange
+            var itemList = new List<Item>();
+
+            var item = new Item()
+            {
+                Name = "Orange",
+                Quality = 10,
+                SellIn = 10
+            };
+
+            itemList.Add(item);
+
+            var guildedRose = new GildedRose.GildedRose(itemList);
+
+            // Act
+            guildedRose.UpdateQuality();
+
+            // Assert
+            Assert.Equal(9, itemList[0].Quality);
+            Assert.Equal(9, itemList[0].SellIn);
+        }
+
+        [Fact]
+        public void GivenSellInDatePassed_QualityDegradesTwice()
+        {
+
         }
     }
 }
